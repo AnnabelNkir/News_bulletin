4ff56647828aae9a8bc34e4417a73424c6d1999c
AnnabelNkir <annabel.micheni@student.moringaschool.com>
2021-11-01 02:57:52 +0300

    This commit modifies the config.py file

--- config.py
+++ config.py
@@ -1,25 +1,23 @@
-class Config:
-    '''
-    General configuration parent class
-    '''
-    NEWS_API_BASE_URL="https://newsapi.org/v2/everything?q=tesla&from=2021-09-30&sortBy=publishedAt&apiKey=1dac542d43124d64a55021524dac25b4"
+import os
 
+
+class Config:
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?language-en&category={}&apiKey={}'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
+
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
+
