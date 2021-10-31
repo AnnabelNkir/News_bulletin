b0f5954b1d58b4a7129ca4773115c422a65c197c
AnnabelNkir <annabel.micheni@student.moringaschool.com>
2021-11-01 02:27:18 +0300

    This commit adds the two models; the sources and the articles model

--- app/models.py
+++ app/models.py
@@ -1,42 +1,21 @@
-class News:
-    '''
-    News class to define News Objects
-    '''
+class Sources:
 
-    def __init__(self,id,name,author, title,description,url):
-        self.id =id
+    def __init__(self, id, name, description, url, category, country, language):
+        self.id = id
         self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        self.language = language
+
+class Articles:
+
+    def __init__(self, id, author, title, description, url, image, date):
+        self.id = id
         self.author = author
         self.title = title
         self.description = description
         self.url = url
-
-class Review:
-
-    all_reviews = []
-
-    def __init__(self,movie_id,title,imageurl,review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
-
-
-    def save_review(self):
-        Review.all_reviews.append(self)
-
-
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
-
-    @classmethod
-    def get_reviews(cls,id):
-
-        response = []
-
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
-
-        return response
+        self.image = image
+        self.date = date
